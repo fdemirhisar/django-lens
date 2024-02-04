2db7e2e59b24bd36788a8f5df2fa270495c1e0df
fdemirhisar <150920925+fdemirhisar@users.noreply.github.com>
2024-02-04 12:29:55 +0100

    Add files via upload

---
+++ project/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
